07f422c9174b75c09e9a8ede1fab13b40c7ca6f1
exerussus <solovyov.production@gmail.com>
2022-09-08 15:29:26 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rrlib',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/exerussus/resp-req_lib_adder.git',
+    license='',
+    author='exerussus',
+    author_email='solovyov.production@gmail.com',
+    description=''
+)
